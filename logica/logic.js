f1a6096aaa2c9c09d48fe4fd85b99b202f0a632e
David Velez Ramirez <davidvelez2605@gmail.com>
2024-03-04 19:39:29 -0500

    Preentrega 2

--- logica/logic.js
+++ logica/logic.js
@@ -1,3 +1,34 @@
+const cursos = [
+    {id: 1, nombre:'crisis digestiva', tema: 'salir de crisis digestiva', modulos: 3, profesora: 'Maria Ramirez', valor: 27},
+    {id: 2, nombre:'Reset desinflamatorio', tema: 'Detox de tu cuerpo', modulos: 7, profesora: 'Maria Ramirez', valor: 117},
+    {id: 3, nombre:'Consulta 1 a 1', tema: 'consulta personalizada para diagnostico', modulos: 5, profesora: 'Maria Ramirez', valor: 97},
+]
+
+const listaInteresados = []
+
+function saludoNom(nombre){
+    return nombre + " Buen día"
+};
+
+function Interesado(nombre, curso, numCuotas, medPago){
+    this.nombre = nombre
+    this.curso = curso;
+    this.numCuotas = numCuotas;
+    this.medPago = medPago;
+
+    this.mostrarInteresados = function(){
+        return(
+            "Nombre interesado: " + this.nombre + "\n"+
+            "Curso de interes: " + this.curso + "\n" +
+            "Numero de cuotas: " + this.numCuotas + "\n" + 
+            "Medio de pago preferido: " + this.medPago + "\n" 
+        )
+    }
+}
+
+
+
+
 let nombre = prompt("Cual es tu nombre");
 
 while (nombre === '') {
@@ -5,35 +36,54 @@ while (nombre === '') {
     nombre = prompt("Cual es tu nombre");
 }
 
-function saludoNom(nom){
-    return nom + " Buen día"
-};
-
-saludoNom(nombre);
+alert(saludoNom(nombre))
 
 let bienvenidaTienda = alert("Bienvenido a Vitalis Academy " + nombre + " a continuacion podras ver nuestro portafolio de cursos");
 
-let productos = prompt("Vitalis Academy te ofrece estas alternativas para sanar tu cuerpo: \n 1) Reset Desinflamatorio \n 2) Consulta Personalizada \n 3) Crisis Digestiva \n 4) No estoy listo/a en este momento \n 5) No quiero informacion ");
+let productos; 
 
-
-
-while (productos != "5") {
+do {
+    productos = prompt("Vitalis Academy te ofrece estas alternativas para sanar tu cuerpo: \n 1) Reset Desinflamatorio \n 2) Consulta Personalizada \n 3) Crisis Digestiva \n 4) No estoy listo/a en este momento \n 5) No quiero informacion/ya finalice ");
     
     switch (productos) {
-        case "1": alert("Has seleccionado el Reset desinflamatorio");
-                    alert("Nos pondremos en contacto contigo")
+        case "1": 
+            alert("Has seleccionado el Reset desinflamatorio");
+            let nuevoInteresado = new Interesado(
+                    nombre,
+                    prompt("Confirmanos en que curso estas interesado"),
+                    parseInt(prompt("Cuantas cuotas estan bien para ti?")),
+                    prompt("que medio de pago deseas aplicar"),
+                    );
+                    listaInteresados.push(nuevoInteresado);
+                    alert(nuevoInteresado.mostrarInteresados())
             break;
 
-        case "2": alert("Has seleccionado la consulta personalizada");
-                    alert("Nos pondremos en contacto contigo")
+        case "2": 
+            alert("Has seleccionado la consulta personalizada");
+            let nuevoInteresado2 = new Interesado(
+                nombre,
+                prompt("Confirmanos en que curso estas interesado"),
+                parseInt(prompt("Cuantas cuotas estan bien para ti?")),
+                prompt("que medio de pago deseas aplicar"),
+                );
+                listaInteresados.push(nuevoInteresado2);
+                alert(nuevoInteresado2.mostrarInteresados())    
             break;
 
-        case "3": alert("Has seleccionado Crisis Digestiva");
-                    alert("Nos pondremos en contacto contigo")
+        case "3": 
+            alert("Has seleccionado Crisis Digestiva");
+            let nuevoInteresado3 = new Interesado(
+                nombre,
+                prompt("Confirmanos en que curso estas interesado"),
+                parseInt(prompt("Cuantas cuotas estan bien para ti?")),
+                prompt("que medio de pago deseas aplicar"),
+                );
+                listaInteresados.push(nuevoInteresado3);
+                alert(nuevoInteresado3.mostrarInteresados())    
             break;
 
         case "4": alert("Entiendo, Quieres que te contactemos despues?");
-                let contacto = alert(" \n Si \n No")
+                let contacto = prompt(" \n Si \n No")
                 if (contacto == "Si") {
                     alert("Espera nuestro mensaje")
                 } else {
@@ -44,8 +94,20 @@ while (productos != "5") {
         default: 
             alert("Selecciona una opcion correcta")
             break;
-    }
-    productos = prompt("Vitalis Academy te ofrece estas alternativas para sanar tu cuerpo: \n 1) Reset Desinflamatorio \n 2) Consulta Personalizada \n 3) Crisis Digestiva \n 4) No estoy listo/a ");
-}
+    } 
+}while (productos != "5")  
 
 alert('Gracias ' + nombre + ' por creer en vitalis academy')
+
+for (const elemento of listaInteresados){
+    console.log("Medios de pago mas solicitados: " + elemento.medPago)
+}
+
+// esto son ejemplos de metodos que me gustaria usar cuando pinte en el html, este con una lupa en el nav
+let buscarCurso = prompt("Que curso te interesa mas")
+
+const buscar = cursos.find((curso) =>{
+    return curso.nombre
+})
+
+console.log(listaInteresados);
